7d53f1def39725124713d66b14d21b83e7cae5ed
Adriana Piccolo <134702809+AdrianaP00@users.noreply.github.com>
2023-06-09 20:45:57 +0000

    add currency input control

--- index.js
+++ index.js
@@ -1,14 +1,29 @@
-const prompt = require('prompt-sync') (); 
+const prompt = require('prompt-sync')();
 
 const amount = parseFloat(prompt('Enter the amount to convert: '));
-const fromCurrency = prompt('Enter the currency to convert from (EUR, USD, GBP, JPY): ').toUpperCase();
-const toCurrency = prompt('Enter the currency to convert to (EUR, USD, GBP, JPY): ').toUpperCase();
+
+let fromCurrency = 0;
+do {
+  fromCurrency = prompt('Enter the currency to convert from (EUR, USD, GBP, JPY): ').toUpperCase();
+} while (fromCurrency != 'EUR' &&
+         fromCurrency != 'USD' &&
+         fromCurrency != 'GBP' &&
+         fromCurrency != 'JPY');
+
+let toCurrency = 0;
+do {
+  toCurrency = prompt('Enter the currency to convert to (EUR, USD, GBP, JPY): ').toUpperCase();
+} while (toCurrency != 'EUR' &&
+         toCurrency != 'USD' &&
+         toCurrency != 'GBP' &&
+         toCurrency != 'JPY');
+
 console.log(convertCurrency(amount, fromCurrency, toCurrency));
 
 function convertCurrency(amount, fromCurrency, toCurrency) {
-  
+
   let exchangeRate = 0;
-  
+
   switch (fromCurrency) {
     case 'EUR':
       switch (toCurrency) {
@@ -22,7 +37,7 @@ function convertCurrency(amount, fromCurrency, toCurrency) {
           exchangeRate = 130.0;
           break;
         default:
-          return NaN; 
+          return NaN;
       }
       break;
 
@@ -30,41 +45,41 @@ function convertCurrency(amount, fromCurrency, toCurrency) {
       switch (toCurrency) {
         case 'EUR':
           exchangeRate = 0.85;
-        break;
+          break;
         case 'GBP':
           exchangeRate = 0.73;
-        break;
+          break;
         case 'JPY':
           exchangeRate = 110.0;
           break;
         default:
-          return NaN; 
+          return NaN;
       }
       break;
 
     case 'GBP':
       switch (toCurrency) {
         case 'EUR':
-          exchangeRate = 1.18; 
+          exchangeRate = 1.18;
           break;
         case 'USD':
-          exchangeRate = 1.37;  
+          exchangeRate = 1.37;
           break;
         case 'JPY':
           exchangeRate = 150.0;
           break;
         default:
-          return NaN; 
+          return NaN;
       }
       break;
 
     case 'JPY':
       switch (toCurrency) {
         case 'EUR':
-          exchangeRate = 0.0077;  
+          exchangeRate = 0.0077;
           break;
         case 'USD':
-          exchangeRate = 0.0091; 
+          exchangeRate = 0.0091;
           break;
         case 'GBP':
           exchangeRate = 0.0067;
@@ -75,7 +90,7 @@ function convertCurrency(amount, fromCurrency, toCurrency) {
       break;
 
     default:
-      return NaN; 
+      return NaN;
   }
 
   return amount * exchangeRate;
